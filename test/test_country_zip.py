97a2e8a27e11798d49c437dbaf3882bb9e0b17de
resteve <resteve@zikzakmedia.com>
2012-07-18 12:03:57 +0200

    Add test Country Zip module

---
+++ test/test_country_zip.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#This file is part helloword module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains 
+#the full copyright notices and license terms.
+
+import sys
+import os
+DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
+    '..', '..', '..', '..', '..', 'trytond')))
+if os.path.isdir(DIR):
+    sys.path.insert(0, os.path.dirname(DIR))
+
+import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT, test_view,\
+    test_depends
+from trytond.transaction import Transaction
+
+class CountryZipTestCase(unittest.TestCase):
+    '''
+    Test Country Zip module.
+    '''
+
+    def setUp(self):
+        trytond.tests.test_tryton.install_module('country_zip')
+
+    def test0005views(self):
+        '''
+        Test views.
+        '''
+        test_view('country_zip')
+
+    def test0006depends(self):
+        '''
+        Test depends.
+        '''
+        test_depends()
+
+
+def suite():
+    suite = trytond.tests.test_tryton.suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        CountryZipTestCase))
+    return suite
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite())
