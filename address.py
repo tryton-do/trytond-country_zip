260d037c5af65bc5c9746f8a86d81e3fc5f0b54a
resteve <resteve@zikzakmedia.com>
2012-11-05 18:18:55 +0100

    Remove import SQL, Views

--- address.py
+++ address.py
@@ -3,7 +3,7 @@
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
 
-from trytond.model import ModelView, ModelSQL, fields
+from trytond.model import fields
 from trytond.pyson import Eval
 from trytond.pool import Pool, PoolMeta
 
