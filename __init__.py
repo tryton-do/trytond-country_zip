1a2890f42241d6875796b959570d53bab565b879
resteve <resteve@zikzakmedia.com>
2014-11-19 09:35:45 +0100

    Migrate version 3.4

--- __init__.py
+++ __init__.py
@@ -3,13 +3,10 @@
 #the full copyright notices and license terms.
 from trytond.pool import Pool
 from .configuration import *
-from .country import *
 from .address import *
-
 
 def register():
     Pool.register(
         Configuration,
         Address,
-        CountryZip,
         module='country_zip', type_='model')
